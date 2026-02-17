e3659f1a24937fb44847d5e730879e1ba0ca6b40
Mehmet Konukçu <mehmetext68@gmail.com>
2026-02-17 22:19:00 +0300

    feat: add Prisma integration and update dependencies

---
+++ src/modules/prisma/prisma.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'src/generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg(process.env.DATABASE_URL);
+    super({ adapter });
+  }
+}
