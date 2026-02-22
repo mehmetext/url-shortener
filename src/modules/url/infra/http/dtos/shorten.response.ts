f7d76f3eff9622b5493607f8030bdfdd0314ee67
Mehmet Konukçu <mehmetext68@gmail.com>
2026-02-22 20:58:54 +0300

    feat: add Swagger documentation for shorten URL response in UrlController

---
+++ src/modules/url/infra/http/dtos/shorten.response.ts
@@ -0,0 +1,54 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ShortenUrlResponse {
+  @ApiProperty({
+    description: 'The ID of the shortened URL',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  id: string;
+
+  @ApiProperty({
+    description: 'The original URL',
+    example: 'https://www.google.com',
+  })
+  originalUrl: string;
+
+  @ApiProperty({
+    description: 'The short code of the shortened URL',
+    example: 'abc123',
+  })
+  shortCode: string;
+
+  @ApiProperty({
+    description: 'The expiration date of the shortened URL (optional)',
+    example: new Date().toISOString(),
+    nullable: true,
+  })
+  expiresAt?: Date | null;
+
+  @ApiProperty({
+    description: 'The user ID of the shortened URL (optional)',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    nullable: true,
+  })
+  userId?: string | null;
+
+  @ApiProperty({
+    description: 'The created at date of the shortened URL',
+    example: new Date().toISOString(),
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    description: 'The updated at date of the shortened URL',
+    example: new Date().toISOString(),
+  })
+  updatedAt: Date;
+
+  @ApiProperty({
+    description: 'The deleted at date of the shortened URL (optional)',
+    example: new Date().toISOString(),
+    nullable: true,
+  })
+  deletedAt?: Date | null;
+}
--- src/modules/url/infra/http/url.controller.ts
+++ src/modules/url/infra/http/url.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiCreatedResponse } from '@nestjs/swagger';
 import { ShortenUrlUseCase } from '../../application/use-cases/ShortenUrl.use-case';
 import { UrlVO } from '../../domain/value-objects/Url.vo';
 import { ShortenUrlDto } from './dtos/shorten.dto';
+import { ShortenUrlResponse } from './dtos/shorten.response';
 
 @Controller('url')
 export class UrlController {
   constructor(private readonly shortenUrlUseCase: ShortenUrlUseCase) {}
 
   @Post('shorten')
+  @ApiCreatedResponse({ type: ShortenUrlResponse })
   async shortenUrl(@Body() body: ShortenUrlDto) {
     const url = new UrlVO(body.originalUrl);
 
