ca994b70c5ee526d3c47bb82052e6a00a5b78162
Mehmet Konukçu <mehmetext68@gmail.com>
2026-02-23 00:53:58 +0300

    refactor: remove unused Url property from User entity and corresponding repository methods

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -1,4 +1,3 @@
-import { Url } from 'src/modules/url/domain/entities/Url.entity';
 import { EmailVO } from '../value-objects/email.vo';
 
 export class User {
@@ -6,7 +5,6 @@ export class User {
     public readonly id: string | undefined,
     public readonly email: EmailVO,
     public readonly password: string,
-    public readonly urls: Url[],
     public readonly createdAt: Date,
     public readonly updatedAt: Date,
     public readonly deletedAt: Date | undefined,
