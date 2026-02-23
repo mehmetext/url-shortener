0864a98c8c99abb5c3e0f011bafba9febe54d741
Mehmet Konukçu <mehmetext68@gmail.com>
2026-02-23 20:48:57 +0300

    feat: add environment variable validation and global validation pipe to enhance request handling

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { BaseResponseInterceptor } from './shared/interceptors/base-response.interceptor';
 
 async function bootstrap() {
+  if (!process.env.PORT) {
+    throw new Error('PORT is not set');
+  }
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -11,13 +16,29 @@ async function bootstrap() {
     .setDescription('The URL Shortener API description')
     .setVersion('1.0')
     .addTag('url-shortener')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors) => {
+        const errorMessages = errors.flatMap((error) =>
+          Object.values(error.constraints ?? {}),
+        );
+
+        return new BadRequestException(errorMessages);
+      },
+    }),
+  );
+
   app.useGlobalInterceptors(new BaseResponseInterceptor());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT);
 }
 
 void bootstrap();
