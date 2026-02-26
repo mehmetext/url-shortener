c656817b0725b934f87c7fee5aba34657d94bfd6
Mehmet Konukçu <mehmetext68@gmail.com>
2026-02-26 20:48:53 +0300

    feat: add cache invalidation for click count in DeleteUrlUseCase to enhance performance by removing cached click data upon URL deletion

--- src/modules/url/application/use-cases/delete-url.use-case.ts
+++ src/modules/url/application/use-cases/delete-url.use-case.ts
@@ -1,5 +1,6 @@
 import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject } from '@nestjs/common';
+import { CLICK_COUNT_CACHE_KEY } from 'src/modules/click/application/config/click-cache.config';
 import { UrlNotFoundError } from '../../domain/errors';
 import { UrlRepository } from '../../domain/repositories/url.repository';
 import { URL_CACHE_KEY } from '../config/url-cache.config';
@@ -20,6 +21,9 @@ export class DeleteUrlUseCase {
     }
 
     await this.cacheManager.del(URL_CACHE_KEY(url.shortCode.value));
+    if (url.id) {
+      await this.cacheManager.del(CLICK_COUNT_CACHE_KEY(url.id));
+    }
 
     await this.urlRepository.delete(command.id);
   }
