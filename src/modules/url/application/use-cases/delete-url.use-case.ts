643692d5aaf4b2d31109b88dddfea663c6e339ec
Mehmet Konukçu <mehmetext68@gmail.com>
2026-02-26 20:25:39 +0300

    feat: implement cache invalidation in DeleteUrlUseCase to enhance performance by removing cached URL entries upon deletion

--- src/modules/url/application/use-cases/delete-url.use-case.ts
+++ src/modules/url/application/use-cases/delete-url.use-case.ts
@@ -1,11 +1,15 @@
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject } from '@nestjs/common';
 import { UrlNotFoundError } from '../../domain/errors';
 import { UrlRepository } from '../../domain/repositories/url.repository';
+import { URL_CACHE_KEY } from '../config/url-cache.config';
 import { DeleteUrlCommand } from '../dtos/delete-url.command';
 
 export class DeleteUrlUseCase {
   constructor(
     @Inject(UrlRepository) private readonly urlRepository: UrlRepository,
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
   ) {}
 
   async execute(command: DeleteUrlCommand) {
@@ -14,6 +18,8 @@ export class DeleteUrlUseCase {
     if (!url) {
       throw new UrlNotFoundError();
     }
+
+    await this.cacheManager.del(URL_CACHE_KEY(url.shortCode.value));
 
     await this.urlRepository.delete(command.id);
   }
